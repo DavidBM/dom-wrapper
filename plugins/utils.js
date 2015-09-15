712391ad8e22ae10c396f41aafe4e594170c6cc3
David Bonet <webbonet@gmail.com>
2015-09-15 14:08:38 +0200

    added offset plugin

--- plugins/utils.js
+++ plugins/utils.js
@@ -24,7 +24,20 @@ function save (obj, name) {
 	return this;
 }
 
+function getOffset() {
+	var el = this.element;
+	var x = 0;
+	var y = 0;
+	while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
+		x += el.offsetLeft - el.scrollLeft;
+		y += el.offsetTop - el.scrollTop;
+		el = el.offsetParent;
+	}
+	return { top: y, left: x };
+}
+
 module.exports = function (engine) {
+	engine.injectPlugin('offset', getOffset);
 	engine.injectPlugin('var', variable);
 	engine.injectPlugin('save', save);
 };
